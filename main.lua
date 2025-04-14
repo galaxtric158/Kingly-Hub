860a96f51c6c461d1227f2b3cbfcd87795b02de3
galaxtric158 <galaxtric158@gmail.com>
2025-04-14 17:24:01 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -4,6 +4,3 @@
 -- TSBG
 if game.PlaceId == 10449761463 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/galaxtric158/Kingly-Hub/refs/heads/main/main.lua"))()
--- Fisch
-if game.placeID == 16732694052 then
-    
