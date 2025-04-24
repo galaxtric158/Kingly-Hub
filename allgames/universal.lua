f3a5927aacff605db1e18bfa5902d8e5370dc2bb
galaxtric158 <galaxtric158@gmail.com>
2025-04-24 18:53:30 +0700

    Update universal.lua

--- allgames/universal.lua
+++ allgames/universal.lua
@@ -10,7 +10,7 @@ local Window = Luna:CreateWindow({
 
     
 	ConfigSettings = {
-		ConfigFolder = "Kingly Hub" -- The Name Of The Folder Where Luna Will Store Configs For This Script. DO NOT ADD A SLASH
+		ConfigFolder = "Kingly Hub"
 	},
 
 
@@ -20,30 +20,30 @@ local Window = Luna:CreateWindow({
 -- TAB CREATION
 
 Window:CreateHomeTab({
-	SupportedExecutors = {}, -- A Table Of Executors Your Script Supports. Add strings of the executor names for each executor.
-    DiscordInvite = "jR8Yt7cbq9", -- The Discord Invite Link. Do Not Include discord.gg/ | Only Include the code.
-	Icon = 1, -- By Default, The Icon Is The Home Icon. If You would like to change it to dashboard, replace the interger with 2
+	SupportedExecutors = {},
+    DiscordInvite = "jR8Yt7cbq9", -- i dont have a discord server haha
+	Icon = 1, --
 })
 
 local ReadMe = Window:CreateTab({
 	Name = "Read Me!",
 	Icon = "description",
 	ImageSource = "Material",
-	ShowTitle = true -- This will determine whether the big header text in the tab will show
+	ShowTitle = true
 })
 
 local Universal = Window:CreateTab({
 	Name = "Universal Scripts",
 	Icon = "account_circle",
 	ImageSource = "Material",
-	ShowTitle = true -- This will determine whether the big header text in the tab will show
+	ShowTitle = true
 })
 
 local Settings = Window:CreateTab({
 	Name = "Settings",
 	Icon = "settings",
 	ImageSource = "Material",
-	ShowTitle = true -- This will determine whether the big header text in the tab will show
+	ShowTitle = true
 })
 
 
@@ -53,10 +53,10 @@ local Settings = Window:CreateTab({
 -- settings stuff
 local Label = Settings:CreateLabel({
 	Text = "Warning! Themes currently do not work.", -- rn themes arent correctly implemented in luna's api. gotta wait for a patch (future galax pls update ts)
-	Style = 3 -- Luna Labels Have 3 Styles : A Basic Label, A Green Information Label and A Red Warning Label. Look At The Following Image For More Details
+	Style = 3
 })
 
-Settings:BuildThemeSection() -- Tab Should be the name of the tab you are adding this section to.
+Settings:BuildThemeSection()
 
 
 
@@ -67,8 +67,8 @@ Universal:CreateSection("Player")
 -- walkspeed
 local Slider = Universal:CreateSlider({
     Name = "WalkSpeed Slider",
-    Range = {16, 1000}, -- The Minimum And Maximum Values Respectively
-    Increment = 1, -- Basically The Changing Value/Rounding Off
+    Range = {16, 1000}, -- the min and max values
+    Increment = 1, -- the changing values/rounding offs
     CurrentValue = 16, -- The Starting Value
     Callback = function(Value)
         local Players = game:GetService("Players")
@@ -92,15 +92,15 @@ local Slider = Universal:CreateSlider({
         -- Set the walk speed immediately when the slider changes
         setWalkSpeed(Value)
     end
-}, "Slider") -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+}, "Slider")
 
 -- jump power slider
 
 local Slider = Universal:CreateSlider({
     Name = "Jump Power Slider",
-    Range = {50, 500}, -- The Minimum And Maximum Values Respectively
-    Increment = 5, -- Basically The Changing Value/Rounding Off
-    CurrentValue = 50, -- The Starting Value
+    Range = {50, 500},
+    Increment = 5,
+    CurrentValue = 50,
     Callback = function(Value)
         local Players = game:GetService("Players")
         local LocalPlayer = Players.LocalPlayer
@@ -123,8 +123,7 @@ local Slider = Universal:CreateSlider({
         -- Set the jump power immediately when the slider changes
         setJumpPower(Value)
     end
-}, "JumpPowerSlider") -- Changed the flag to be more descriptive
-
+}, "JumpPowerSlider")
 
 
 -- scripts
@@ -132,44 +131,33 @@ Universal:CreateSection("Scripts")
 
 local Button = Universal:CreateButton({
 	Name = "Load Infinite Yield",
-	Description = "Loads the script Infinite Yield.", -- Creates A Description For Users to know what the button does (looks bad if you use it all the time),
+	Description = "Loads the script Infinite Yield.", -- an admin script thing idk broo
     	Callback = function()
             loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
-         -- The function that takes place when the button is pressed
     	end
 })
 
 local Button = Universal:CreateButton({
 	Name = "Load ESP",
-	Description = "Loads an ESP script.", -- Creates A Description For Users to know what the button does (looks bad if you use it all the time),
+	Description = "Loads an ESP script.", -- esp wowo
     	Callback = function()
             loadstring(game:HttpGet("https://raw.githubusercontent.com/Lucasfin000/SpaceHub/main/UESP"))()
-         -- The function that takes place when the button is pressed
     	end
 })
 
 Universal:CreateSection("Fisch")
 local Button = Universal:CreateButton({
 	Name = "Load Speed Hub X",
-	Description = "Loads the script hub Speed Hub X.", -- Creates A Description For Users to know what the button does (looks bad if you use it all the time),
+	Description = "Loads the script hub Speed Hub X.", -- fisch
     	Callback = function()
             loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
-         -- The function that takes place when the button is pressed
-    	end
-})
-local Button = Universal:CreateButton({
-	Name = "Load Ronix Hub",
-	Description = "Loads the script hub Ronix for Fisch.", -- Creates A Description For Users to know what the button does (looks bad if you use it all the time),
-    	Callback = function()
-            loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/aa63ce25cd44fa60f0b155dcc3593445.lua"))()
-         -- The function that takes place when the button is pressed
     	end
 })
 
 -- readme stuff
 local Label = ReadMe:CreateLabel({
 	Text = "Information",
-	Style = 2 -- Luna Labels Have 3 Styles : A Basic Label, A Green Information Label and A Red Warning Label. Look At The Following Image For More Details
+	Style = 2
 })
 
 local Paragraph = ReadMe:CreateParagraph({
