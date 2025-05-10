9412a8c2b6b573f673b3bb0570ccc46c2b705efa
galax <galaxtric158@gmail.com>
2025-05-10 11:27:34 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -5,5 +5,5 @@
 if game.PlaceId == 10449761463 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/galaxtric158/Kingly-Hub/refs/heads/main/allgames/TheStrongestBattlegrounds.lua"))()
 else 
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/galaxtric158/Kingly-Hub/refs/heads/main/allgames/universal.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/galaxtric158/Kingly-Hub/refs/heads/main/allgames/universalscripts/universal.lua"))()
 end
